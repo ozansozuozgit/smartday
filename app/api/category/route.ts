021f051e1290d8a8055dc66ac533418dff732a06
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-06-11 19:20:01 -0500

    add category id

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -4,25 +4,26 @@ import { useParams } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
 
-export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
+// export async function GET(req: NextRequest) {
+//   const session = await getServerSession(authOptions);
 
-  const allCategoriesFromUser = await prisma.category.findMany({
-    where: { userId: session?.user?.id },
-  });
+//   const allCategoriesFromUser = await prisma.user.findMany({
+//     where: { userId: session?.user?.id },
+//   });
 
-  // const allCategoriesFromUser = res.json();
-  console.log('allCategoriesFromUser', allCategoriesFromUser);
+//   // const allCategoriesFromUser = res.json();
+//   console.log('allCategoriesFromUser', allCategoriesFromUser);
 
-  return NextResponse.json(allCategoriesFromUser);
-}
+//   return NextResponse.json(allCategoriesFromUser);
+// }
 
 export async function POST(req: NextRequest) {
   const { categoryName } = await req.json();
-
+  const session = await getServerSession(authOptions);
   const newCategory = await prisma.category.create({
     data: {
       name: categoryName, // replace with the actual activity name
+      userId: session?.user?.id,
     },
   });
 
