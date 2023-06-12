3d9915a51873ac03dbce8f0c67e33ecea1f3aab6
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-06-11 19:34:46 -0500

    categories for specific user

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -5,9 +5,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
 
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
+  const session: any = await getServerSession(authOptions);
 
-  const allCategoriesFromUser = await prisma.category.findMany();
+  const allCategoriesFromUser = await prisma.category.findMany({
+    where: { userId: session?.user?.id },
+  });
 
   // const allCategoriesFromUser = res.json();
   console.log('allCategoriesFromUser', allCategoriesFromUser);
