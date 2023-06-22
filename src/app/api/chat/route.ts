6aa8fa793764c98df8e81914c6370923f999bb91
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-06-21 19:20:16 -0500

    test push

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -7,6 +7,7 @@ import { Configuration, OpenAIApi } from 'openai-edge';
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(config);
 
 // IMPORTANT! Set the runtime to edge
