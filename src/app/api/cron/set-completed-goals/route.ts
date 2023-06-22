10c598c603e1082c815b4332f459a9fd15e70144
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-06-21 20:10:39 -0500

    test

--- src/app/api/cron/set-completed-goals/route.ts
+++ src/app/api/cron/set-completed-goals/route.ts
@@ -1,8 +1,6 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
-export const config = {
-  runtime: 'edge',
-}
+export const runtime = 'edge';
 
 export async function GET() {
   try {
