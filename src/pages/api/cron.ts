f4e86c46ef492492b56307c34ab10361419ad16a
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-06-21 20:28:57 -0500

    test

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,7 +1,11 @@
 import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req: NextRequest) {
   try {
     // Retrieve all users
     const users = await prisma.user.findMany();
